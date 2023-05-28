b6337ff77765aea81e44b5959e275ca5f719f79c
chcustodio97@gmail.com <chcustodio97@gmail.com>
2023-05-28 19:54:37 -0300

    grpc

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.18
 require github.com/gin-gonic/gin v1.9.0
 
 require (
-	cloud.google.com/go/compute/metadata v0.2.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 )
 
 require (
@@ -38,6 +39,7 @@ require (
 	golang.org/x/oauth2 v0.7.0
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/grpc v1.55.0
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,32 +1,57 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
+	"log"
 
-	connectDB "github.com/UserFinder/connect"
-	"github.com/UserFinder/controllers"
-	"github.com/UserFinder/initializers"
-	"github.com/gin-gonic/gin"
+	pb "github.com/UserFinder/models"
+
+	"google.golang.org/grpc"
 )
 
-// Main function
-func init() {
-	initializers.LoadEnv()
-	connectDB.InitConnector()
-}
+ 
 
 func main() {
 
-	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
-	})
-
-	r.GET("/migrate", controllers.Migrate)
-	r.GET("/getUser", controllers.GetUser)
-	r.POST("/postUser", controllers.PostCreate)
-	r.Run(os.Getenv("port"))
 	fmt.Println("Hello World")
+		// Set up a connection to the gRPC server
+		conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Failed to connect to the server: %v", err)
+		}
+		defer conn.Close()
+	
+		// Create a client instance for the UserService
+		client := pb.NewUserServiceClient(conn)
+	
+		// Example: Create a user
+		req := &pb.UserRequest{
+			Name:     "John Doe",
+			Email:    "john@example.com",
+			Password: "password123",
+		}
+	
+		
+		response, err := client.CreateUser(context.Background(), req)
+		if err != nil {
+			log.Fatalf("Failed to create user: %v", err)
+		}
+	
+		log.Printf("User created successfully. ID: %s", response.Id)
+	
+		// Example: Get a user
+		req = &pb.UserRequest{
+			Email:    "john@example.com",
+			Password: "password123",
+		}
+	
+		response, err = client.GetUser(context.Background(), req)
+		if err != nil {
+			log.Fatalf("Failed to get user: %v", err)
+		}
+	
+		log.Printf("User retrieved successfully. ID: %s, Name: %s", response.Id, response.Name)
+	
 }
